59afa08391107da92c1ec0852db76a7a8e25c779
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-03-17 01:33:58 -0300

    Add banner list section

---
+++ sections/BannerList.tsx
@@ -0,0 +1,25 @@
+export interface ListItem {
+    text: string;
+    bold?: boolean;
+    color?: "text-red-800" | "text-green-800";
+}
+
+export interface Props {
+    title: string;
+    description?: string;
+    items: ListItem[];
+}   
+
+export default function BannerList(props: Props) {
+    return (
+        <div className="bg-primary p-4">
+            <h1 className="text-2xl font-bold">{props.title}</h1>
+            {props.description && <p className="text-gray-600">{props.description}</p>}
+            <ul className="mt-4">
+                {props.items.map((item, index) => (
+                    <li key={index} className={`${item.color ?? "text-gray-800"}`}>{item.text}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
